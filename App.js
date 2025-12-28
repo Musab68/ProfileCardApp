52c668d100371a36bbbd3be59daaf65002190781
Musab68 <musaboz109@gmail.com>
2025-12-28 11:54:25 +0300

    feat: create theme system with design tokens (Part 2)

--- App.js
+++ App.js
@@ -1,33 +1,16 @@
 import { useFonts } from 'expo-font';
-import { View, Text, StyleSheet } from 'react-native';
+import ProfileScreen from './screens/ProfileScreen'; // Yeni oluşturduğumuz ekranı çağırıyoruz [cite: 149]
 
 export default function App() {
-  // Fontları yüklüyoruz [cite: 30]
   const [fontsLoaded] = useFonts({
-    'Montserrat': require('./assets/fonts/Montserrat-Regular.ttf'), // [cite: 31]
-    'MontserratBold': require('./assets/fonts/Montserrat-Bold.ttf'), // [cite: 32]
+    'Montserrat': require('./assets/fonts/Montserrat-Regular.ttf'),
+    'MontserratBold': require('./assets/fonts/Montserrat-Bold.ttf'),
   });
 
-  // Fontlar yüklenene kadar boş bir ekran döndür [cite: 34, 35]
   if (!fontsLoaded) {
     return null;
   }
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Fonts Loaded!</Text>
-    </View>
-  );
+  // Artık ana ekranda ProfileScreen bileşenini döndürüyoruz [cite: 159]
+  return <ProfileScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center', // [cite: 48]
-    alignItems: 'center',     // [cite: 49]
-  },
-  text: {
-    fontFamily: 'MontserratBold', // Ödevde belirtilen font ismi [cite: 53]
-    fontSize: 24,                // [cite: 54]
-  },
-});
